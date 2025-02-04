86c0152523d83cf6ccafcabbbbfdd54de7e8e082
Louise McMahon <louise@louisemcmahon.co.uk>
2025-02-04 13:07:04 +0000

    Fix NPM build

--- app/Data/UserData.php
+++ app/Data/UserData.php
@@ -10,7 +10,7 @@ class UserData extends Data
 {
     public function __construct(
         public string $id,
-        public ?string $emailAddress,
+        public string $emailAddress,
         public ?string $emailVerifiedAt,
         public array|Lazy $members,
         public DiscordUserData|Lazy $discordUser,
@@ -20,7 +20,7 @@ class UserData extends Data
     {
         return new self(
             id: $user->id,
-            emailAddress: $user->email,
+            emailAddress: $user->email ?? '',
             emailVerifiedAt: $user->email_verified_at ? $user->email_verified_at->toDateString() : null,
             members: Lazy::whenLoaded(
                 'members',
